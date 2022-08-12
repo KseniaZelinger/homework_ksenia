0d81492390149ab126cf718998aa8f44c4cafcf0
KseniaZelinger <zelingerksenia@gmail.com>
2022-08-12 08:47:19 +0300

    новое задание

---
+++ 11.09.22/index.js
@@ -0,0 +1,40 @@
+// let d = 8;
+// function soneFunction() {
+// if (d === 8) {
+// const b = '10'
+// var c = 21;
+// let a = 5;
+// }
+// }
+
+//console.log(c);
+//console.log(a);
+//console.log(b);
+
+// let arr = [1, 2, 3, 4, 5, [1, 1, 1], true];
+// let obj = {
+//     first: 1,
+//     'second': 5,
+//     3: 'third',
+//     4: [1, 2, 3],
+//     5: {
+//         moscow: 777,
+//         spb: 767,
+//     }
+
+// }
+// console.log(arr[0]);
+// console.log(obj.first);
+
+// for (const key in obj) {
+//     console.log(obj[key])
+// }
+
+const employer = {
+    name: "Сергей Вальтер",
+    say() {
+        console.log('hello!');
+    }
+}
+console.log(employer)
+console.log(employer.say())
